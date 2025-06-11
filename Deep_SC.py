7f0c73c86301ceb2df7f39a526ef56f5e9bff813
Weiyue Li <90059662+usernameAI@users.noreply.github.com>
2025-06-12 02:22:10 +0800

    Update Deep_SC.py

--- Deep_SC.py
+++ Deep_SC.py
@@ -5,9 +5,140 @@ from recbole.model.init import xavier_uniform_initialization
 from recbole.model.loss import BPRLoss
 from recbole.utils import InputType
 from recbole_gnn.model.abstract_recommender import SocialRecommender
-from torch_geometric.nn.conv import HypergraphConv, TransformerConv, SuperGATConv
+from torch_geometric.nn.conv import SuperGATConv
 import torch.nn.functional as F
 from recbole_gnn.model.layers import LightGCNConv
+from torch import Tensor
+from torch.nn import Parameter
+from torch_geometric.experimental import disable_dynamic_shapes
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.nn.dense.linear import Linear
+from torch_geometric.nn.inits import glorot, zeros
+from torch_geometric.utils import scatter, softmax
+
+class HypergraphConv(MessagePassing):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        use_attention: bool = False,
+        attention_mode: str = 'node',
+        heads: int = 1,
+        concat: bool = True,
+        negative_slope: float = 0.2,
+        dropout: float = 0,
+        bias: bool = True,
+        **kwargs,
+    ):
+        kwargs.setdefault('aggr', 'add')
+        super().__init__(flow='source_to_target', node_dim=0, **kwargs)
+
+        assert attention_mode in ['node', 'edge']
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.use_attention = use_attention
+        self.attention_mode = attention_mode
+
+        if self.use_attention:
+            self.heads = heads
+            self.concat = concat
+            self.negative_slope = negative_slope
+            self.dropout = dropout
+            self.lin = Linear(in_channels, heads * out_channels, bias=False,
+                              weight_initializer='glorot')
+            self.att = Parameter(torch.empty(1, heads, 2 * out_channels))
+        else:
+            self.heads = 1
+            self.concat = True
+            self.lin = Linear(in_channels, out_channels, bias=False,
+                              weight_initializer='glorot')
+
+        if bias and concat:
+            self.bias = Parameter(torch.empty(heads * out_channels))
+        elif bias and not concat:
+            self.bias = Parameter(torch.empty(out_channels))
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        super().reset_parameters()
+        self.lin.reset_parameters()
+        if self.use_attention:
+            glorot(self.att)
+        zeros(self.bias)
+
+    @disable_dynamic_shapes(required_args=['num_edges'])
+    def forward(self, x: Tensor, hyperedge_index: Tensor,
+                hyperedge_weight: Optional[Tensor] = None,
+                hyperedge_attr: Optional[Tensor] = None,
+                num_edges: Optional[int] = None) -> Tensor:
+        num_nodes = x.size(0)
+
+        if num_edges is None:
+            num_edges = 0
+            if hyperedge_index.numel() > 0:
+                num_edges = int(hyperedge_index[1].max()) + 1
+
+        if hyperedge_weight is None:
+            hyperedge_weight = x.new_ones(num_edges)
+
+        x = self.lin(x)
+
+        alpha = None
+        if self.use_attention:
+            assert hyperedge_attr is not None
+            x = x.view(-1, self.heads, self.out_channels)
+            hyperedge_attr = self.lin(hyperedge_attr)
+            hyperedge_attr = hyperedge_attr.view(-1, self.heads,
+                                                 self.out_channels)
+            x_i = x[hyperedge_index[0]]
+            x_j = hyperedge_attr[hyperedge_index[1]]
+            alpha = (torch.cat([x_i, x_j], dim=-1) * self.att).sum(dim=-1)
+            # alpha = F.leaky_relu(alpha, self.negative_slope)
+            alpha = F.gelu(alpha)
+            if self.attention_mode == 'node':
+                alpha = softmax(alpha, hyperedge_index[1], num_nodes=num_edges)
+            else:
+                alpha = softmax(alpha, hyperedge_index[0], num_nodes=num_nodes)
+            alpha = F.dropout(alpha, p=self.dropout, training=self.training)
+
+        D = scatter(hyperedge_weight[hyperedge_index[1]], hyperedge_index[0],
+                    dim=0, dim_size=num_nodes, reduce='sum')
+        D = 1.0 / D
+        D[D == float("inf")] = 0
+
+        B = scatter(x.new_ones(hyperedge_index.size(1)), hyperedge_index[1],
+                    dim=0, dim_size=num_edges, reduce='sum')
+        B = 1.0 / B
+        B[B == float("inf")] = 0
+
+        out = self.propagate(hyperedge_index, x=x, norm=B, alpha=alpha,
+                             size=(num_nodes, num_edges))
+        out = self.propagate(hyperedge_index.flip([0]), x=out, norm=D,
+                             alpha=alpha, size=(num_edges, num_nodes))
+
+        if self.concat is True:
+            out = out.view(-1, self.heads * self.out_channels)
+        else:
+            out = out.mean(dim=1)
+
+        if self.bias is not None:
+            out = out + self.bias
+
+        return out
+
+    def message(self, x_j: Tensor, norm_i: Tensor, alpha: Tensor) -> Tensor:
+        H, F = self.heads, self.out_channels
+
+        out = norm_i.view(-1, 1, 1) * x_j.view(-1, H, F)
+
+        if alpha is not None:
+            out = alpha.view(-1, self.heads, 1) * out
+
+        return out
 
 
 class fusion_triple_feature(nn.Module):
